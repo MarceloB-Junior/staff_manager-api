75f04849cbd6007ebf33880d4ecaf41f7f486585
MarceloB-Junior <marcelo.bernardino@sou.unifal-mg.edu.br>
2025-08-02 15:33:25 -0300

    feat: add documentation to EmployeeController class

--- src/main/java/com/api/staff_manager/controllers/EmployeeController.java
+++ src/main/java/com/api/staff_manager/controllers/EmployeeController.java
@@ -3,17 +3,27 @@ package com.api.staff_manager.controllers;
 import com.api.staff_manager.dtos.requests.EmployeeRequest;
 import com.api.staff_manager.dtos.responses.EmployeeDetailsResponse;
 import com.api.staff_manager.dtos.responses.EmployeeViewResponse;
+import com.api.staff_manager.exceptions.dto.ApiError;
 import com.api.staff_manager.services.PhotoService;
 import com.api.staff_manager.services.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -34,14 +44,68 @@ public class EmployeeController {
     private final EmployeeService employeeService;
     private final PhotoService photoService;
 
+    @Operation(
+            summary = "Get all employees",
+            description = "Get all employees from the Staff Manager API",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieved employees",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PagedModel.class),
+                            examples = @ExampleObject(value =
+                                    "{\"content\":[{\"employee_id\":\"f89ef3d9-0fe4-488f-b228-8addbb326db3\"," +
+                                            "\"name\":\"string\",\"position\":\"string\"," +
+                                            "\"salary\":7500.00," +
+                                            "\"department_id\":\"3fa85f64-5717-4562-b3fc-2c963f66afa6\"}]," +
+                                            "\"page\":{\"size\":10,\"number\":0,\"totalElements\":1,\"totalPages\":1}}"
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+            )
+    })
     @GetMapping
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<Page<EmployeeViewResponse>> getAllEmployees(
+    public ResponseEntity<Page<EmployeeViewResponse>> getAllEmployees(@ParameterObject
             @PageableDefault(sort = "employeeId", direction = Sort.Direction.ASC) Pageable pageable){
         log.info("Request received to fetch all employees");
         return ResponseEntity.ok(employeeService.findAll(pageable));
     }
 
+    @Operation(
+            summary = "Get one employee by id",
+            description = "Get an employee from the Staff Manager API",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved employee",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = EmployeeDetailsResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Employee not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    )
+            }
+    )
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<EmployeeDetailsResponse> getEmployeeById(@PathVariable(value = "id")UUID id){
@@ -49,6 +113,34 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeService.findById(id));
     }
 
+    @Operation(
+            summary = "Get an employee photo by id",
+            description = "Get an employee photo from the Staff Manager API",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved employee photo",
+                            content = {
+                                    @Content(mediaType = "image/png"),
+                                    @Content(mediaType = "image/jpeg"),
+                                    @Content(mediaType = "image/jpg")
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Employee not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    )
+            }
+    )
     @GetMapping("/{id}/photo")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Resource> getEmployeePhotoById(@PathVariable(value = "id") UUID id,
@@ -59,6 +151,43 @@ public class EmployeeController {
         return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
     }
 
+    @Operation(
+            summary = "Create a new employee",
+            description = "Create a new employee from the Staff Manager API",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Successfully created an employee",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = EmployeeViewResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Employee already exists",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Department not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    )
+            }
+    )
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EmployeeViewResponse> saveEmployee(@RequestBody @Valid EmployeeRequest request){
@@ -66,6 +195,43 @@ public class EmployeeController {
         return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.save(request));
     }
 
+    @Operation(
+            summary = "Update an employee",
+            description = "Update an employee from the Staff Manager API",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully updated an employee",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = EmployeeDetailsResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Another employee with the same name already exists in department",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Employee/Department not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    )
+            }
+    )
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EmployeeDetailsResponse> updateEmployee(@PathVariable(value = "id") UUID id,
@@ -74,6 +240,34 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeService.update(request, id));
     }
 
+    @Operation(
+            summary = "Delete an employee",
+            description = "Delete an employee from the Staff Manager API",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Successfully deleted an employee"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Employee not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    )
+            }
+    )
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteEmployee(@PathVariable(value = "id") UUID id){
@@ -82,7 +276,35 @@ public class EmployeeController {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/{id}/photo")
+    @Operation(
+            summary = "Upload an employee photo",
+            description = "Create a new employee photo from the Staff Manager API",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully created an employee photo"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Employee not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    )
+            }
+    )
+    @PostMapping(value = "/{id}/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EmployeeDetailsResponse> uploadPhoto(@PathVariable(value = "id") UUID id,
                                                                @RequestParam("file") MultipartFile file){
@@ -93,6 +315,34 @@ public class EmployeeController {
         return ResponseEntity.ok(photoService.associatePhotoToEmployee(id, photoPath));
     }
 
+    @Operation(
+            summary = "Delete an employee photo",
+            description = "Delete an employee from the Staff Manager API",
+            security = @SecurityRequirement(name = "Bearer Authentication")
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Successfully deleted an employee photo"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Employee not found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))
+                    )
+            }
+    )
     @DeleteMapping("/{id}/photo")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deletePhotoByEmployeeId(@PathVariable(value = "id") UUID id){
